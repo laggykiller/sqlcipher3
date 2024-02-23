746bd4ea1856be1221dfbb5e05e305d5f62fa106
laggykiller <61652821+laggykiller@users.noreply.github.com>
2024-02-24 03:59:46 +0800

    Linting and fix build

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,5 @@
 from conan import ConanFile
-import shutil
 
 class OpensslRecipe(ConanFile):
     def requirements(self):
-        self.requires('openssl/1.1.1w')
+        self.requires("openssl/1.1.1w")
