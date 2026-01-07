fa661035cc29b6d076c9bca8f7af2ebf27132a99
laggykiller <chaudominic2@gmail.com>
2026-01-07 11:50:11 +0800

    0.5.7

--- setup.py
+++ setup.py
@@ -183,7 +183,7 @@ if __name__ == "__main__":
         # With pyproject.toml, all are not necessary except ext_modules
         # However, they are kept for building python 3.6 wheels
         name="sqlcipher3-wheels",
-        version="0.5.6",
+        version="0.5.7",
         package_dir={"sqlcipher3": "sqlcipher3"},
         packages=["sqlcipher3"],
         ext_modules=[module],
